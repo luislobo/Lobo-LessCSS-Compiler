1391afd6ecadf6c17d50d3bdbac86cfffac3bf18
Luis Lobo Borobia <lobo@fictioncity.net>
2012-03-12 00:59:06 -0300

    First working version. Threading removed.

--- LLCC.py
+++ LLCC.py
@@ -24,11 +24,13 @@ class MainWindow(wx.Frame):
 	def __init__(self, parent, title):
 
 		# Initializes pyinotify objects
-		self.wm = pyinotify.WatchManager()
+		self.wm = None
 		self.notifier = None
+		self.stopWatching = 0
+		self.directories = dict()
 		
 		# Create main frame
-		wx.Frame.__init__(self, parent, title=title, size=(400, 500))
+		wx.Frame.__init__(self, parent, title=title, size=(500, 500))
 
 		# Init the GUI
 		self.InitGUI()
@@ -46,13 +48,16 @@ class MainWindow(wx.Frame):
 		
 		# add button to pannel
 		hbButtons = wx.BoxSizer(wx.HORIZONTAL)
-		self.addButton = wx.Button(self.mainPanel, 1, "Add Directory to watch")
-		self.startButton = wx.ToggleButton(self.mainPanel, 2, "Start watching")
-		hbButtons.Add(self.addButton, 2, flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
-		hbButtons.Add(self.startButton, 2, flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
+		self.addDirectoryButton = wx.Button(self.mainPanel, 1, "Add Directory to watch")
+		self.removeDirectoryButton = wx.Button(self.mainPanel, 2, "Remove Directory from list")
+		self.startWatchingButton = wx.ToggleButton(self.mainPanel, 3, "Start watching")
+		hbButtons.Add(self.addDirectoryButton, 2, flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
+		hbButtons.Add(self.removeDirectoryButton, 2, flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
+		hbButtons.Add(self.startWatchingButton, 2, flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
 
 		# Add config list to panel
 		self.configList = ConfigListBox(self.mainPanel)
+		self.ReadDirectories()
 
 		# Add controls to vertical box sizer
 		vbox = wx.BoxSizer(wx.VERTICAL)
@@ -81,10 +86,17 @@ class MainWindow(wx.Frame):
 
 		# Set events.
 		self.Bind(wx.EVT_MENU, self.OnExit, self.menuExit)
-		self.Bind(wx.EVT_BUTTON, self.OnAdd, self.addButton)
-		self.Bind(wx.EVT_TOGGLEBUTTON, self.OnStartWatching, self.startButton)
+		self.Bind(wx.EVT_BUTTON, self.OnAddDirectory, self.addDirectoryButton)
+		self.Bind(wx.EVT_BUTTON, self.OnRemoveDirectory, self.removeDirectoryButton)
+		
+		self.Bind(wx.EVT_TOGGLEBUTTON, self.OnStartWatching, self.startWatchingButton)
 		self.Bind(wx.EVT_CLOSE, self.OnClose)
 
+	def ReadDirectories(self):
+		self.directories = dict()
+		for directory in self.configList.Items:
+			self.directories[directory] = None
+	
 	def OnClose(self, event):
 		# stop notifier
 		self.StopWatching()
@@ -92,49 +104,74 @@ class MainWindow(wx.Frame):
 		self.Destroy()
 
 	def StartWatching(self):
-		# TODO: handle a better way to see if thread is running
-		if self.notifier is None:
-			self.notifier = pyinotify.ThreadedNotifier(self.wm, EventHandler())
-			self.notifier.start()
-			for directory in self.configList.Items:
-				self.AddPathToWatch(directory)
-				self.setSBMessage("Adding: " + directory)
-			self.setSBMessage("Watching...")
+		self.stopWatching = 0
+		if self.wm is None:
+			self.wm = pyinotify.WatchManager()
+			if self.notifier is None:
+				self.notifier = pyinotify.Notifier(self.wm, EventHandler(),timeout=10)
+				self.ReadDirectories()
+				for k in self.directories.keys():
+					self.AddPathToWatch(k)
+					self.setSBMessage("Adding: " + k)
+				self.setSBMessage("Watching...")
+				
+				# process Events
+				while True:
+					if self.stopWatching:
+						if not self.wm is None:
+							self.notifier = None
+							self.wm.close()
+							self.wm = None
+							self.directories = dict()
+						self.setSBMessage("Not Watching...")					
+						break
+					self.notifier.process_events()
+					if self.notifier.check_events():
+						self.notifier.read_events()
+					wx.Yield()
 		
 	def StopWatching(self):
 		# TODO: handle a better way to see if thread is running
-		if not self.notifier is None:
-			for directory in self.wm.watches():
-				self.wm.rm_watch(directory, True, True)
-			self.notifier.stop()
-			self.notifier = None
-		self.setSBMessage("Not Watching...")
+		self.stopWatching = 1
 
 	def AddPathToWatch(self, path):
 		# watched events
+		if not path in self.directories:
+			self.configList.Append(path)
+			self.configList.saveList()
 		mask = pyinotify.IN_CLOSE_WRITE
-		self.wdd = self.wm.add_watch(path, mask, rec=True)
+		self.directories[path] = self.wm.add_watch(path, mask, rec=False)
 
-	def OnAdd(self, event):
+	def OnAddDirectory(self, event):
 		self.OpenDirectory()
+		
+	def OnRemoveDirectory(self,event):
+		self.RemoveSelectedDirectory()
+	
+	def RemoveSelectedDirectory(self):
+		sel = self.configList.GetSelection()
+		if not sel == wx.NOT_FOUND:
+			directory = self.configList.Items[sel]
+			if self.directories[directory] != None:
+				self.wm.rm_watch(self.directories[directory],rec=False)
+			del self.directories[directory]
+			self.configList.Delete(sel)
 
 	def OnStartWatching(self, event):
 		# TODO: handle a better way to see if thread is running
 		# If it's pressed, means it is already running
-		startIsPressed = self.startButton.GetValue()
+		startIsPressed = self.startWatchingButton.GetValue()
 
 		if startIsPressed:
-			self.startButton.SetLabel("Stop Watching")
+			self.startWatchingButton.SetLabel("Stop Watching")
 			self.StartWatching()
 		else:
-			self.startButton.SetLabel("Start Watching")
+			self.startWatchingButton.SetLabel("Start Watching")
 			self.StopWatching()
 		
 	def OpenDirectory(self):
 		dlg = wx.DirDialog(self, "Choose a directory:", style=wx.DD_DEFAULT_STYLE)
 		if dlg.ShowModal() == wx.ID_OK:
-			self.configList.Append(dlg.GetPath())
-			self.configList.saveList()
 			self.AddPathToWatch(dlg.GetPath())
 		dlg.Destroy()
 		
@@ -146,7 +183,9 @@ class EventHandler(pyinotify.ProcessEvent):
 		fileName, fileExtension = os.path.splitext(event.pathname)
 		if fileExtension == ".less":
 			frame.setSBMessage("Compiling: " + fileName + '.css')
-			#os.system('lessc -x %s > %s' % (event.pathname, fileName + '.css'))
+			command = 'lessc -x %s > %s' % (event.pathname, fileName + '.css')
+			#alert(command)
+			os.system(command)
 			frame.setSBMessage("Compiled: " + fileName + '.css')
 
 class ConfigListBox(wx.ListBox):
